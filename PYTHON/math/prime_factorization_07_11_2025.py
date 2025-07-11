c8de2b6bcdffbb36bb41a5770e1d982535086213
Roman Retsen <vatraltd@gmail.com>
2025-07-11 11:25:42 -0400

    Python->math->prime

---
+++ PYTHON/math/prime_factorization_07_11_2025.py
@@ -0,0 +1,44 @@
+"""
+Every number has a unique prime factorisation.
+https://www.mathsisfun.com/prime-factorization.html
+
+Example
+12 = 2×2×3
+
+2310 = 2×3×5×7×11
+
+For a given input number, your program must output the sum of the numbers in the unique prime factorisation of that number.
+Input
+Line 1: An integer N greater than 1
+Output
+1 line The sum of the prime factors of N
+Constraints
+1<N<15000
+Example
+Input
+
+60
+
+Output
+
+12
+
+----------
+11
+
+11
+--------------
+1086
+
+186
+------------
+13230
+
+30
+
+"""
+
+
+
+n = int(input())
+
