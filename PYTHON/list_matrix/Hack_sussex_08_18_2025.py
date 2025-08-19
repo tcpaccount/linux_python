23521477a4f4d0f126598e975b32ab4ce944a958
RomanRetsen <59456345+RomanRetsen@users.noreply.github.com>
2025-08-18 21:54:08 -0400

    Update Hack_sussex_08_18_2025.py
    added 3-d test case

--- PYTHON/list_matrix/Hack_sussex_08_18_2025.py
+++ PYTHON/list_matrix/Hack_sussex_08_18_2025.py
@@ -21,4 +21,5 @@ def solution(paths):
 if __name__ == "__main__":
     sample_paths1 = [["London", "Paris"], ["Paris", "Lima"], ["Lima", "San Paulo"]]
     sample_paths2 = [["Buffalo", "Baltimore"], ["Toronto", "Buffalo"], ["Baltimore", "Washington"]]
+    sample_paths3 = [["Berlin", "Sofia"], ["Milan", "Berlin"], ["Sofia", "Kyiv"]]
     print(solution(sample_paths1))
