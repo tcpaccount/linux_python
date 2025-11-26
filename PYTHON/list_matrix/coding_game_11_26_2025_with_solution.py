e5b75654aae15a97978bd659285d94b82d082d1d
TPC Account <tpc_account@proton.me>
2025-11-26 15:23:07 -0500

    added to list module

---
+++ PYTHON/list_matrix/coding_game_11_26_2025_with_solution.py
@@ -0,0 +1,79 @@
+'''
+You are given a string s. Your task is to reverse the characters located at the odd indices of s (0-based indexing). The even-indexed characters remain in place.
+
+Example:
+s = ABCDEFG
+Odd-index characters are B, D, F.
+Reversed: F, D, B.
+Put this back in and you get AFCDEBG
+Input
+Line 1: A string s.
+Output
+Line 1: The string s after reversing the odd indices.
+Constraints
+2 = length of s = 150
+s only contains ASCII characters
+Example
+Input
+ABCDEFGH
+Output
+AHCFEDGB
+________________________
+azbycxdwevfu
+
+output
+aubvcwdxeyfz
+________________________
+!@#$%^&*()[]{};':",.<>/?\|_+
+
+output
+!+#|%?&>(.["{';}:],)<*/^\$_@
+________________________
+Aa$%&#$^ &@$b  slyh a w687.640654|{}{FHsiduflauljJOLoa76A761SSS  SSSSSSDDDDD DDDD gsfga   odgiua oigua he llo
+
+output
+Al$ &h$a g@oba ildh a wg8s. 4D6D4D{D{DHSiSuSl uSj1O7o67aAL6JSlSa fSdSsSFD}D| 5D0D6g7f6a   oygsu  $i&u^ #e%lao
+________________________
+0123456789101112131415161718192021222232425262728293031323334353637383940.654
+
+output
+052.4463831313131313131313121222222222324251607988970615243342516079879503614
+________________________
+5  3
+
+output
+53  
+________________________
+ /
+
+output
+ /
+________________________
+
+________________________
+
+
+
+'''
+
+import sys
+import math
+from itertools import zip_longest
+
+s = input()  # Only contains ASCII characters
+
+odd = []
+even = []
+the_result = []
+for i, c in enumerate(s):
+    if not i % 2:
+        even.append(c)
+    else:
+        odd.append(c)
+for a, b in zip_longest(odd[::-1], even):
+    if b:
+        the_result.append(b)
+    if a:
+        the_result.append(a)
+print("".join(the_result))
+
