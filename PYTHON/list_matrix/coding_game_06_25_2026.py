5505c385886f943db2422e0805d3e5163b253e42
Roman Retsen <vatraltd@gmail.com>
2026-05-26 14:01:47 -0400

    Fibonachi reversed

---
+++ PYTHON/list_matrix/coding_game_06_25_2026.py
@@ -0,0 +1,43 @@
+'''
+You must output the reverse of the Fibonacci function.
+Fib(X) = Y
+With Fib(1) = Fib(2) = 1
+And Fib(N) = Fib(N - 1) + Fib(N - 2)
+Input
+Long Integer Y
+Output
+Integer X
+Constraints
+0 < X < 2^16
+0 < Y < 2^64
+Example
+Input
+
+13
+
+Output
+
+7
+
+---------------------
+ Test 2
+
+ 233
+
+ 13
+ ---------------------
+ 3
+ Test 3
+
+ 46368
+
+ 24
+ ---------------------
+ Test 4
+
+ 12586269025
+
+ 50
+
+'''
+y = int(input())
